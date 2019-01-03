4efa84719d71d51f0b2ad0db53e0a6b7ae20a974
kuznetsovandrey76 <and.rey.q@yandex.ru>
2019-01-03 12:44:04 +0300

    fix .each on ios

--- assets/js/post.js
+++ assets/js/post.js
@@ -1,16 +1,17 @@
 $( document ).ready(function() {
     const items = ['relax', 'creative', 'career', 'health', 'family', 'rise', 'env', 'spirit'];
-    $.each(items, function(index, value){
-        $('#' + value).hide();
-	});
+    for (var i = 0; i < items.length; ++i) {
+        $('#' + items[i]).hide();
+	}
 
     $('.nav__item').click(function() {
         let select_item = $(this).find('p').text();
         console.log(select_item);
 
-        $.each(items, function(index, value){
-            $('#' + value).hide();
-        });
+        for (var i = 0; i < items.length; ++i) {
+            $('#' + items[i]).hide();
+        }
         $('#' + select_item).show();
     })
   });
+
